lhoursquentin/sed-bin

commit 2941c4193f55e2eb12aadb3a6b388b7459479943

move generated code in main
will be easier for implementing the d command

par.sed
@@ -42,10 +42,10 @@ s/^b[[:blank:]]*//; t b_cmd
 s/^t[[:blank:]]*//; t t_cmd
 s/^:[[:blank:]]*//; t label_cmd
 s/^s//; t s_cmd
-s/^[dDhHgGlnNpPx]/&(status);\
+s/^[dDhHgGlnNpPx]/&(\&status);\
 /
 t single_char_cmd
-s/^=/equal(status);\
+s/^=/equal(\&status);\
 /
 t single_char_cmd
 s/^q/exit(0);\
@@ -282,7 +282,7 @@ h
 # The very top of the hold contains the info needed to generate the correct
 # function name
 s/^\([^[:space:]]*\).*\
-/\1(status, /
+/\1(\&status, /
 s/^[nr].*/if (addr_&)/
 p
 # clean the C code from the hold
@@ -310,7 +310,7 @@ b fail
 : t_cmd
 
 # eat what we will process right now, what remains is the future current line
-s/^[^[:blank:];}][^[:blank:];}]*/if (status->sub_success) { status->sub_success = false; goto &; }\
+s/^[^[:blank:];}][^[:blank:];}]*/if (\&status->sub_success) { status.sub_success = false; goto &; }\
 /
 t valid_t_parsing
 s/^/t command parsing: /

sed-bin.c
@@ -8,11 +8,6 @@
 #include "read.h"
 #include "status.h"
 
-void sed_script(Status *status) {
-  #include "generated.c"
-  return;
-}
-
 int main(int argc, char **argv) {
   Status status = {
     .pattern_space = (char[PATTERN_SIZE]){},
@@ -22,9 +17,9 @@ int main(int argc, char **argv) {
   };
 
   while (read_pattern(&status)) {
-    sed_script(&status);
-    puts(status.pattern_space);
     // FIXME reset substitution success value
+    #include "generated.c"
+    puts(status.pattern_space);
   }
   return EXIT_SUCCESS;
 }

test
@@ -48,6 +48,7 @@ verify 's/xz/nope/' 'Hello World'
 verify 's/\([^ ]*\)/\1! \1/' 'Hello World'
 verify 's/\([^ ]*\) \(.\)/\1! \2 /' 'Hello World'
 verify 's/.*r/& & &/' 'Hello World'
+verify 's/Hello/\& literal ampersand/' 'Hello World'
 # g opt
 verify 's/o/a/g' 'Hello World'
 verify 's/o/abocde/g' 'Hello World'

commit d586b1bf8dc54175933236b5343729a92bb92cb1

sed-bin: fix t cmd broken by 2941c41 refactor

par.sed
@@ -310,7 +310,7 @@ b fail
 : t_cmd
 
 # eat what we will process right now, what remains is the future current line
-s/^[^[:blank:];}][^[:blank:];}]*/if (\&status->sub_success) { status.sub_success = false; goto &; }\
+s/^[^[:blank:];}][^[:blank:];}]*/if (status.sub_success) { status.sub_success = false; goto &; }\
 /
 t valid_t_parsing
 s/^/t command parsing: /