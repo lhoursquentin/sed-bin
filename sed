lhoursquentin/sed-bin

commit d560d2d0353be478ce09827ab94703545d7d1708

misc: allow to name final binary

sed
@@ -7,16 +7,16 @@ An implementation of sed based on C translation.
 
 Usage:
 
-  sed [−n] script [file...]
+  sed [-n] script [file...]
 
-  sed [−n] −e script [−e script]... [−f script_file]... [file...]
+  sed [-n] -e script [-e script]... [-f script_file]... [file...]
 
-  sed [−n] [−e script]... −f script_file [−f script_file]...  [file...]
+  sed [-n] [-e script]... -f script_file [-f script_file]...  [file...]
 EOF
   exit "${1-0}"
 }
 
-bin=./sed-bin
+bin="${BIN-./sed-bin}"
 default_translator=./par.sed
 translator="${SED_TRANSLATOR-$default_translator}"
 generated_file=generated.c
@@ -79,5 +79,13 @@ printf '%s\n' "$script" | "$translator" > "$generated_file" &&
     if "$n_opt_found"; then
       set -- -n
     fi
-    "$bin" "$@"
+    case "$bin" in
+      /*)
+        set -- "$bin"
+        ;;
+      *)
+        set -- ./"$bin"
+        ;;
+    esac
+    "$@"
   }

Makefile
@@ -1,9 +1,9 @@
 objs = address.o operations.o read.o sed-bin.o
-bin = sed-bin
+BIN ?= sed-bin
 
-$(bin): $(objs)
-	# the line below is implicit with GNU make, add it for BSD compatibility
-	$(CC) $(CFLAGS) $(objs) -o $@
+$(BIN): $(objs)
+	@# the line below is implicit with GNU make, add it for BSD compatibility
+	$(CC) $(objs) -o $@
 
 sed-bin.o: generated.c generated-init.c
 

compile
@@ -31,7 +31,7 @@ done
 
 readonly translator=./par.sed
 readonly generated_file=generated.c
-readonly bin=./sed-bin
+readonly bin="${BIN-./sed-bin}"
 
 set -x
 

commit df5a48dc92e73035e43d291414373d511d18e472

sed: fix -n option not taking effect
Regression introduced by d560d2d

sed
@@ -81,10 +81,10 @@ printf '%s\n' "$script" | "$translator" > "$generated_file" &&
     fi
     case "$bin" in
       /*)
-        set -- "$bin"
+        set -- "$bin" "$@"
         ;;
       *)
-        set -- ./"$bin"
+        set -- ./"$bin" "$@"
         ;;
     esac
     "$@"